ac822339d2d10d56d536a87f61b1b8c1665d4cc3
CRjulianTICO <CRjulianTICO@gmail.com>
2018-11-21 11:32:36 -0600

    Initial commit

---
+++ app/src/main/java/org/example/julia/exception/MakeFragment.java
@@ -0,0 +1,22 @@
+package org.example.julia.exception;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MakeFragment extends Fragment {
+
+    public MakeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_make, container, false);
+    }
+}
